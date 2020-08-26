b95be6b622d074396bc659077efe0c55562fbea0
memin <memindmr06@gmail.com>
2020-08-26 12:39:30 +0100

    leapYears solved

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  if (year % 4 === 0) {
+    if (year >= 400 && year % 100 === 0) {
+      if (year % 400) return false;
+    }
+    return true;
+  } else return false;
+};
 
-}
-
-module.exports = leapYears
+module.exports = leapYears;
