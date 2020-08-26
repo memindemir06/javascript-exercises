76ef21fb5e1c3d8af5d03358b1de93382866b78d
memin <memindmr06@gmail.com>
2020-08-26 11:43:23 +0100

    repeatString solved

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+  const adder = str;
+  if (num < 0) {
+    return "ERROR";
+  } else if (num === 0) {
+    return "";
+  } else {
+    for (let i = 0; i < num - 1; i++) {
+      str += adder;
+    }
+  }
+  return str;
+};
 
-}
-
-module.exports = repeatString
+module.exports = repeatString;
