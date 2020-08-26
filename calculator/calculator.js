eb7d7e7b2b747083c9394326b3fb47b359d66d4e
memin <memindmr06@gmail.com>
2020-08-26 13:36:47 +0100

    calculator solved

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,32 +1,47 @@
-function add () {
-	
+function add(a, b) {
+  if (typeof a === "number" && typeof b === "number") return a + b;
+  return "ERROR";
 }
 
-function subtract () {
-	
+function subtract(a, b) {
+  if (typeof a === "number" && typeof b === "number") return a - b;
+  return "ERROR";
 }
 
-function sum () {
-	
+function sum(arr) {
+  let res = arr.reduce((sum, el) => {
+    if (typeof el === "number") sum += el;
+    return sum;
+  }, 0);
+  return res;
 }
 
-function multiply () {
-	
+function multiply(arr) {
+  let res = arr.reduce((sum, el) => {
+    if (typeof el === "number") sum *= el;
+    return sum;
+  }, 1);
+  return res;
 }
 
-function power() {
-	
+function power(a, b) {
+  return Math.pow(a, b);
 }
 
-function factorial() {
-	
+function factorial(num) {
+  if (typeof num !== "number") return "ERROR";
+  let res = 1;
+  for (let i = 1; i <= num; i++) {
+    res *= i;
+  }
+  return res;
 }
 
 module.exports = {
-	add,
-	subtract,
-	sum,
-	multiply,
-    power,
-	factorial
-}
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial,
+};
