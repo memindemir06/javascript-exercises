ed1266c0afa72d538dd0522b63eeb233d02a0f34
memin <memindmr06@gmail.com>
2020-08-26 12:30:28 +0100

    sumAll solved

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (
+    typeof num1 === "number" &&
+    typeof num2 === "number" &&
+    num1 >= 0 &&
+    num2 >= 0
+  ) {
+    let sum = 0;
+    for (let i = Math.min(num1, num2); i <= Math.max(num1, num2); i++) {
+      sum += i;
+    }
+    return sum;
+  } else return "ERROR";
+};
 
-}
-
-module.exports = sumAll
+module.exports = sumAll;
