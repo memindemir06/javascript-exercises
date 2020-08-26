273a0dd6849ce010fbf5c58a13e1f35ee129d803
memin <memindmr06@gmail.com>
2020-08-26 16:44:11 +0100

    getTheTitles solved

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
-}
+const getTheTitles = function (books) {
+  return books.reduce((titles, book) => {
+    titles.push(book.title);
+    return titles;
+  }, []);
+};
 
 module.exports = getTheTitles;
