eefef23b7c2633658c46d2aa9d69572b5074814f
memin <memindmr06@gmail.com>
2020-08-26 13:47:32 +0100

    fibo solved

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (num <= 0) return "OOPS";
+  let arr = [1, 1];
+  if (num === 1) {
+    return 1;
+  }
+  if (num === 2) {
+    return 2;
+  } else {
+    for (let i = 2; i < num; i++) {
+      let newNum = arr[i - 1] + arr[i - 2];
+      arr.push(newNum);
+    }
+  }
+  return arr[arr.length - 1];
+};
 
-}
-
-module.exports = fibonacci
+module.exports = fibonacci;
