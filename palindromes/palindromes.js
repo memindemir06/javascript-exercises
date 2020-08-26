ded7f7af7799b10e0ad6d5225376a4c59373de68
memin <memindmr06@gmail.com>
2020-08-26 17:03:13 +0100

    palindromes solved

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,27 @@
-const palindromes = function() {
+const palindromes = function (word) {
+  word = word.toLowerCase();
+  const reg = /[a-zA-Z]/;
+  if (word.length === 1) return true;
+  let start = 0;
+  let end = word.length - 1;
+  while (start < end) {
+    if (
+      word[start].match(reg) &&
+      word[end].match(reg) &&
+      word[start] !== word[end]
+    ) {
+      return false;
+    } else if (word[start].match(reg) && !word[end].match(reg)) {
+      end--;
+      continue;
+    } else if (!word[start].match(reg) && word[end].match(reg)) {
+      start++;
+      continue;
+    }
+    end--;
+    start++;
+  }
+  return true;
+};
 
-}
-
-module.exports = palindromes
+module.exports = palindromes;
