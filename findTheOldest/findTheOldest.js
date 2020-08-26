db63b64effafd208e0c411de68693cab5642897f
memin <memindmr06@gmail.com>
2020-08-26 16:40:10 +0100

    findTheOldest solved

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-let findTheOldest = function() {
+let findTheOldest = function (people) {
+  const now = new Date();
+  const today = now.getFullYear();
+  let ages = [];
+  return people.reduce((oldest, person) => {
+    const age = person.hasOwnProperty("yearOfDeath")
+      ? person.yearOfDeath - person.yearOfBirth
+      : today - person.yearOfBirth;
+    ages.push(age);
+    oldest = Math.max(...ages) === age ? person : oldest;
+    return oldest;
+  }, {});
+};
 
-}
-
-module.exports = findTheOldest
+module.exports = findTheOldest;
