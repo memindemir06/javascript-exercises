2a14dd6b8ef72d6b4b89e01b51a9df3a2ecab01e
memin <memindmr06@gmail.com>
2020-08-26 12:52:19 +0100

    tempConversion solved

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,14 @@
-const ftoc = function() {
+const ftoc = function (f) {
+  let c = ((f - 32) * 5) / 9;
+  return parseFloat(c.toFixed(1));
+};
 
-}
-
-const ctof = function() {
-
-}
+const ctof = function (c) {
+  let f = (c * 9) / 5 + 32;
+  return parseFloat(f.toFixed(1));
+};
 
 module.exports = {
   ftoc,
-  ctof
-}
+  ctof,
+};
